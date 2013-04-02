9da3454e1a1a18ed0169555ac64ee5f40f2754e2
Olivier Bruchez <olivier@bruchez.org>
2013-04-02 15:59:46 -0700

    Add dependency for POI

--- project/Build.scala
+++ project/Build.scala
@@ -2,15 +2,15 @@ import sbt._
 import sbt.Keys._
 
 object ProjectBuild extends Build {
-
   lazy val root = Project(
     id = "root",
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
       name := "german-analysis",
       organization := "org.bruchez",
-      version := "0.1-SNAPSHOT",
-      scalaVersion := "2.9.2"
+      version := "0.2",
+      scalaVersion := "2.9.3",
+      libraryDependencies += "org.apache.poi" % "poi-ooxml" % "3.9"
       // add other settings here
     )
   )
